897154d78b85a081be21258d7a3f03539ada34f6
Alessio <alessio26manfredini@gmail.com>
2022-03-11 16:53:32 +0100

    Add function to play game comparing value

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,49 @@
+// Get computer Selection 
 function computerPlay() {
     const computerChoice = ["Rock", "Paper", "Scissors"];
-    console.log(computerChoice[Math.floor(Math.random() * computerChoice.length)]);
+    return computerChoice[Math.floor(Math.random() * computerChoice.length)];
+}
+const computerSelection = computerPlay();
+
+
+
+const playerSelection = "Rock";
+
+
+// funciton to compare value and choose winner
+function PlayRound(playerSelection, computerSelection) {
+    switch(playerSelection){
+        case "Rock":
+            if(computerSelection === "Rock"){
+                return "you choosed: " + playerSelection + ", the computer choosed: " + computerSelection + ". Nobody win";
+            }else if(computerSelection === "Paper"){
+                return "you choosed: " + playerSelection + ", the computer choosed: " + computerSelection + ". The computer Win!";
+            }else {
+                return "you choosed: " + playerSelection + ", the computer choosed: " + computerSelection + ". You Win!";
+            }
+
+
+        case "Paper":
+            if(computerSelection === "Paper"){
+                return "you choosed: " + playerSelection + ", the computer choosed: " + computerSelection + ". Nobody win";
+            }else if(computerSelection === "Scissors"){
+                return "you choosed: " + playerSelection + ", the computer choosed: " + computerSelection + ". The computer Win!";
+            }else {
+                return "you choosed: " + playerSelection + ", the computer choosed: " + computerSelection + ". You Win!";
+            }
+
+
+        case "Scissors": 
+            if(computerSelection === "Scissors"){
+                return "you choosed: " + playerSelection + ", the computer choosed: " + computerSelection + ". Nobody win";
+            }else if(computerSelection === "Rock"){
+                return "you choosed: " + playerSelection + ", the computer choosed: " + computerSelection + ". The computer Win!";
+            }else {
+                return "you choosed: " + playerSelection + ", the computer choosed: " + computerSelection + ". You Win!";
+            }
+    }
 }
 
-computerPlay();
+
+const result = PlayRound(playerSelection, computerSelection);
+console.log(result);
